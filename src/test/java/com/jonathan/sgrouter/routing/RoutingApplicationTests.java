771a1c5b0831e8d210b840c644b1e3ee9d1b4db2
jloh02 <36648707+jloh02@users.noreply.github.com>
2021-06-04 14:07:16 +0800

    Developed deployable Springboot Graph Builder Service

---
+++ src/test/java/com/jonathan/sgrouter/routing/RoutingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonathan.sgrouter.routing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
