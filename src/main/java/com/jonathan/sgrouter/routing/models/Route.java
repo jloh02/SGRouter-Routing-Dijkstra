c0e212e5c0b719a9ae0a70ad3ec4875f1b390a94
jloh02 <36648707+jloh02@users.noreply.github.com>
2021-07-10 19:04:15 +0800

    Beautify and truncate output route

--- src/main/java/com/jonathan/sgrouter/routing/models/Route.java
+++ src/main/java/com/jonathan/sgrouter/routing/models/Route.java
@@ -9,13 +9,57 @@ public class Route implements Comparable<Route> {
   double time;
   ArrayList<SubRoute> route;
 
-  // TODO truncate path
   public Route(
       double time, ArrayList<SubRoute> path, HashMap<String, String> names, double lastWalk) {
     this.time = time + lastWalk;
-    this.route = new ArrayList<>(path);
-    for (SubRoute sr : this.route) sr.setName(names.get(sr.getDes()));
-    this.route.add(new SubRoute(lastWalk, "Walk", "Destination"));
+
+    this.route = new ArrayList<>();
+
+    SubRoute curr = new SubRoute();
+    for (int i = 0; i < path.size() - 1; i++) {
+      SubRoute sr = path.get(i), post = path.get(i + 1);
+
+      if (curr.getService().isEmpty()) curr.setService(sr.getService());
+      if (curr.getService().contains("Walk")) curr.setService("Walk");
+
+      curr.addTime(sr.getTime());
+      if (sr.getService().equals(post.getService())
+          || (sr.getService().contains("Walk") && post.getService().contains("Walk")))
+        curr.addInter(sr.getDes(), names.get(sr.getDes()));
+      else {
+        curr.setDes(sr.getDes());
+        curr.setName(names.get(sr.getDes()));
+        this.route.add(curr);
+        curr = new SubRoute();
+      }
+    }
+
+    SubRoute sr = path.get(path.size() - 1);
+    if (curr.getService().isEmpty()) curr.setService(sr.getService());
+    if (curr.getService().contains("Walk")) curr.setService("Walk");
+    curr.addTime(sr.getTime());
+
+    if (curr.getService().equals("Walk")) {
+      curr.addInter(sr.getDes(), sr.getName());
+
+      curr.addTime(lastWalk);
+      curr.setService("Walk");
+      curr.setDes("");
+      curr.setName("Destination");
+
+      this.route.add(curr);
+    } else {
+      curr.setDes(sr.getDes());
+      curr.setName(names.get(sr.getDes()));
+      this.route.add(curr);
+
+      curr = new SubRoute();
+      curr.addTime(lastWalk);
+      curr.setService("Walk");
+      curr.setDes("");
+      curr.setName("Destination");
+      this.route.add(curr);
+    }
   }
 
   @Override
--- src/main/java/com/jonathan/sgrouter/routing/models/SubRoute.java
+++ src/main/java/com/jonathan/sgrouter/routing/models/SubRoute.java
@@ -1,11 +1,14 @@
 package com.jonathan.sgrouter.routing.models;
 
+import java.util.ArrayList;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 public class SubRoute implements Comparable<SubRoute> {
-  double time = Double.MAX_VALUE;
-  String service = "", des = "", name;
+  private double time = Double.MAX_VALUE;
+  private String service = "", des = "", name;
+  private ArrayList<InterPoint> inter = new ArrayList<>();
 
   public SubRoute() {}
 
@@ -15,8 +18,43 @@ public class SubRoute implements Comparable<SubRoute> {
     this.des = des;
   }
 
+  public void addTime(double t) {
+    this.time += t;
+  }
+
+  public void addInter(String id, String name) {
+    inter.add(new InterPoint(id, name));
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder("{time=");
+    sb.append(time);
+    sb.append(", service=");
+    sb.append(service);
+    sb.append(", des=");
+    sb.append(des);
+
+    if (inter.size() != 0) {
+      sb.append(", name=");
+      sb.append(name);
+    }
+
+    if (name != null) {
+      sb.append(", name=");
+      sb.append(name);
+    }
+    return sb.toString();
+  }
+
   @Override
   public int compareTo(SubRoute s) {
     return this.time > s.time ? 1 : -1;
   }
+}
+
+@Data
+@AllArgsConstructor
+class InterPoint {
+  String id, name;
 }
